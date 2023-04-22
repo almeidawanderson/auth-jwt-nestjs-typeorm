f2a088828b990697579da2f2b9db76f178ebe2e0
Wanderson Almeida <wandersonalmeida.eng@gmail.com>
2023-04-22 20:54:04 -0300

    fix: adjustment api-http and auth-service

--- src/auth/auth/auth.service.ts
+++ src/auth/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import {JwtService} from '@nestjs/jwt'
+import { error } from 'console';
 
 
 const users = [
@@ -18,22 +19,20 @@ const users = [
   id: 3,
   username: 'user3@user@.com',
   password: '$2b$10$yG6UxJcIACYNlaSVyn/Cy.tTf0wg8Ya2AQvOcNT9QndRMbK8m/w7O'  
-}
-]
+},
+];
 
 @Injectable()
 export class AuthService {
   constructor(private jwtService: JwtService) {}
 
-  async login(username: string, password: string) {
+  login(username: string, password: string) {
     const user = this.validateCredentials(username, password);
 
     const payload = {
-      sub: user.id,
-      username: user.username      
+      sub: user?.id,
+      name: user?.username        
     }
-
-    console.log(payload);
     return this.jwtService.sign(payload)
     
     
@@ -41,7 +40,12 @@ export class AuthService {
 
   validateCredentials(username: string, password: string) {
     const user = users.find((u => u.username === username 
-      && bcrypt.compareSync(password, u.password))) 
+      && bcrypt.compareSync(password, u.password))
+      );
+      
+      if(!user) {
+        throw new Error('User not found!')
+      }
     return user;
   }
   
